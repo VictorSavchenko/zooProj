83a400bb98820ba49c187f034e3b311f7fa62186
Анна Прокопчук <prokopchuk.anna.21@gmail.com>
2024-06-06 17:19:32 +0500

    done

--- src/views/Tariff.jsx
+++ src/views/Tariff.jsx
@@ -3,47 +3,53 @@ const Layout = require('./Layout');
 
 module.exports = function Tariff({ prices, login }) {
   return (
-    <Layout>
-      <div className='p-3 bg-info bg-opacity-10 border border-info border-start-5 rounded-end div-tariff'>
-        <table className='table-tariff'>
-          <thead>
-            <tr className='tr-tariff'>
-              <th className='th-tariff'>Тариф</th>
-              <th className='th-tariff'>Цена</th>
-              {login && <th>Изменение цены</th>}
-            </tr>
-          </thead>
-          <tbody>
-            {login
-              ? prices.map((price) => (
-                  <tr className='tr-tariff' key={price.id}>
-                    <td className='td-tariff'>{price.tariff}</td>
-                    <td className='td-tariff' name={`cost-${price.id}`}>{price.cost}</td>
-                    <td>
-                      <input type='text' className='form-control' />
-                    </td>
-                    <td>
-                      <button
-                        type='submit'
-                        id={price.id}
-                        type='button'
-                        className='btn btn-light changeBtn'
-                      >
-                        Изменить
-                      </button>
-                    </td>
-                  </tr>
-                ))
-              : prices.map((price) => (
-                  <tr className='tr-tariff'>
-                    <td className='td-tariff'>{price.tariff}</td>
-                    <td className='td-tariff'>{price.cost}</td>
-                  </tr>
-                ))}
-          </tbody>
-        </table>
+    <Layout login={login}>
+      <div className='container'>
+      <div className='table-container'>
+        <div className='p-3 bg-info bg-opacity-10 border border-info border-start-5 rounded-end div-tariff'>
+          <table className='table-tariff'>
+            <thead>
+              <tr className='tr-tariff'>
+                <th className='th-tariff'>Тариф</th>
+                <th className='th-tariff'>Цена</th>
+                {login && <th>Изменение цены</th>}
+              </tr>
+            </thead>
+            <tbody>
+              {login
+                ? prices.map((price) => (
+                    <tr className='tr-tariff' key={price.id}>
+                      <td className='td-tariff'>{price.tariff}</td>
+                      <td className='td-tariff' name={`cost-${price.id}`}>
+                        {price.cost}
+                      </td>
+                      <td>
+                        <input type='text' className='form-control' />
+                      </td>
+                      <td>
+                        <button
+                          type='submit'
+                          id={price.id}
+                          type='button'
+                          className='btn btn-light form-button form-button-success changeBtn'
+                        >
+                          Изменить
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                : prices.map((price) => (
+                    <tr className='tr-tariff'>
+                      <td className='td-tariff'>{price.tariff}</td>
+                      <td className='td-tariff'>{price.cost}</td>
+                    </tr>
+                  ))}
+            </tbody>
+          </table>
+        </div>
       </div>
       <link rel='stylesheet' href='/css/tariff.css' />
+      </div>
       {login && <script src='/js/changeTarrifs.js' />}
     </Layout>
   );
